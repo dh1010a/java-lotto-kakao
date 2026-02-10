5d3da697789d655509759ac5dd8683822ed79ebc
jbw9964 <jbw9964@gmail.com>
2026-02-10 15:59:34 +0900

    refactor: `InputHandler` 를 이용한 `Main` class 리팩터

--- src/main/java/level1/Main.java
+++ src/main/java/level1/Main.java
@@ -1,57 +1,48 @@
 package level1;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
-    private static final Scanner scanner;
+    private static final InputHandler inputHandler;
     private static final RandomLotteryGenerator randomLotteryGenerator;
 
     static {
-        scanner = new Scanner(System.in);
+        inputHandler = new InputHandler();
         randomLotteryGenerator = new RandomLotteryGenerator();
     }
 
     public static void main(String[] args) {
-        int price = inputPrice();
+        int price = inputHandler.inputPrice();
+
         Lotteries userLotteries = purchaseLotteries(price);
+
         printLotteries(userLotteries);
 
-        AnswerLottery answerLottery = inputAnswerLottery();
-        printResult(price, userLotteries, answerLottery);
-    }
+        AnswerLottery answerLottery = inputHandler.inputAnswerLottery();
 
-    private static int inputPrice() {
-        System.out.println("구입금액을 입력해 주세요.");
-        return Integer.parseInt(scanner.nextLine());
+        Report report = new Report(
+                price, answerLottery, userLotteries.getLotteries()
+        );
+
+        printResult(report);
     }
 
     private static Lotteries purchaseLotteries(int price) {
         int count = price / Constant.LOTTERY_PRICE;
+
         System.out.println(count + "개를 구매했습니다.");
+
         return randomLotteryGenerator.generate(count);
     }
 
     private static void printLotteries(Lotteries lotteries) {
         System.out.println(lotteries.representLotteries());
+
         System.out.println();
     }
 
-    private static AnswerLottery inputAnswerLottery() {
-        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
-        List<String> numbers = Arrays.asList(scanner.nextLine().split(",\\s*"));
-
-        System.out.println("보너스 볼을 입력해 주세요.");
-        String bonusBall = scanner.nextLine();
-
-        return new AnswerLottery(numbers, bonusBall);
-    }
-
-    private static void printResult(int price, Lotteries userLotteries, AnswerLottery answer) {
-        Report report = new Report(price, answer, userLotteries.getLotteries());
+    private static void printResult(Report report) {
         System.out.println();
+
         System.out.println(report.representReport());
     }
 }
