fcd991fc81675371977bbdf4667b917eeab76496
dh1010a <dh1010a@naver.com>
2026-02-11 16:41:34 +0900

    refactor: price 내 상수 변경

--- src/main/java/level1/Price.java
+++ src/main/java/level1/Price.java
@@ -2,15 +2,13 @@ package level1;
 
 public record Price(int value) {
 
-    private static final int MINIMUM_PRICE = 1000;
-
     public Price {
         validate(value);
     }
 
     private void validate(int value) {
-        if (value < MINIMUM_PRICE) {
-            throw new IllegalArgumentException(MINIMUM_PRICE + "원 이상부터 구매가 가능합니다.");
+        if (value < Constant.LOTTERY_PRICE) {
+            throw new IllegalArgumentException(Constant.LOTTERY_PRICE + "원 이상부터 구매가 가능합니다.");
         }
     }
 }
